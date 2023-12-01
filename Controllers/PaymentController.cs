46e78bf2ae2a2f785e32ad5468356f300213b753
Jose-Rivas-L <jose.rivas537549@gmail.com>
2023-12-01 11:29:26 -0500

    "creacion del webhook y cadena de conexion"

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,5 +1,9 @@
-﻿namespace TransactionTrackerAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransactionTrackerAPI.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class PaymentController
     {
     }
--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -1,18 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.Json;
 using TransactionTrackerAPI.models;
 
 namespace TransactionTrackerAPI.Controllers
 {
+    [ApiController]
+    [Route("api/webhook")]
     public class WebhookController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult ReceiveNotification([FromBody] PaymentNotification notification)
-        {
-            // Lógica para almacenar la notificación en la base de datos
-            _context.NotificationRequests.Add(notification);
-            _context.SaveChanges();
+        private readonly IConfiguration configuration;
 
-            return Ok();
+        public WebhookController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("save")]
+        public IActionResult HandleWebhook(PaymentNotification webhookData)
+        {
+            try
+            {
+                int requestId = webhookData.RequestId;
+                string payerName = webhookData.Request.Payer.Name;
+                string payerSurname = webhookData.Request.Payer.Surname;
+                string payerDocument = webhookData.Request.Payer.Document;
+                string payerEmail = webhookData.Request.Payer.Email;
+                string payerMobile = webhookData.Request.Payer.Mobile;
+                string paymentStatus = webhookData.Payments[0].Status.Status;
+                string paymentMessage = webhookData.Payments[0].Status.Message;
+                DateTime paymentDate = webhookData.Payments[0].Status.Date;
+                int paymentAmount = webhookData.Payments[0].Amount.Total;
+                string paymentBank = webhookData.Payments[0].IssuerName;
+                string paymentMethod = webhookData.Payments[0].PaymentMethod;
+
+
+                // Save the info in database
+                Console.WriteLine($"Request ID: {requestId}");               
+                Console.WriteLine($"Payer Document: {payerDocument}");
+                Console.WriteLine($"Payer Name: {payerName}");
+                Console.WriteLine($"Payer Surname: {payerSurname}");
+                Console.WriteLine($"Payer Email: {payerEmail}");
+                Console.WriteLine($"Payer Mobile: {payerMobile}");
+                Console.WriteLine($"Payment status: {paymentStatus}");
+                Console.WriteLine($"Payment Message: {paymentMessage}");
+                Console.WriteLine($"Payment Date: {paymentDate}");
+                Console.WriteLine($"Payment Amount: {paymentAmount}");
+                Console.WriteLine($"Payment Bank: {paymentBank}");
+                Console.WriteLine($"Payment Method: {paymentMethod}");
+
+
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // Maneja cualquier excepción que pueda ocurrir durante el procesamiento
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
         }
     }
+
+
 }
